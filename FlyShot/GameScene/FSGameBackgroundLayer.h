da61e5839c3f02efaa3e8830b91a0b801715502d
DAIKI <daiki_chin@pankaku.com>
2012-05-27 17:17:16 +0900

    first setting

---
+++ FlyShot/GameScene/FSGameBackgroundLayer.h
@@ -0,0 +1,16 @@
+//
+//  FSGameBackgroundLayer.h
+//  FlyShot
+//
+//  Created by Daiki on 12/05/27.
+//  Copyright 2012年 Keio University SFC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface FSGameBackgroundLayer : CCLayer {
+    
+}
+
+@end
